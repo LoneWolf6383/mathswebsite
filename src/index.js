d61e80891f135505d3c5b273dc183af614b0a2c9
LoneWolf6383 <83755082+LoneWolf6383@users.noreply.github.com>
2022-01-02 14:36:11 +0530

    update 2
    Still no improvements. White screen error still exists

--- src/index.js
+++ src/index.js
@@ -2,9 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.Fragment><p>hi</p><App /></React.Fragment>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App/>,document.getElementById('root'));
