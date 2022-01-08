86d4fca2828891631da0442e4b5b255f6bb03665
LoneWolf6383 <83755082+LoneWolf6383@users.noreply.github.com>
2022-01-08 11:34:27 +0530

    creating input fields

---
+++ formulations/firstOrderDiffEq/app.js
@@ -0,0 +1,9 @@
+const x1 = document.querySelector('#x1_ip').value
+const y1 = document.querySelector('#y1_ip').value
+const xn = document.querySelector('#xn_ip').value
+const h = document.querySelector('#h_ip').value
+const eq = document.querySelector('#eq_ip').value
+const sub_but = document.querySelector('#sub_but')
+sub_but.addEventListener('click', () => {
+    console.log(x1.value)
+})
