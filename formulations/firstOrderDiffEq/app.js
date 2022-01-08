68deea08b91ae44a36c8ad45c9891a60cbf6e1bf
LoneWolf6383 <83755082+LoneWolf6383@users.noreply.github.com>
2022-01-08 23:03:55 +0530

    added all req fields and funcs

--- formulations/firstOrderDiffEq/app.js
+++ formulations/firstOrderDiffEq/app.js
@@ -1,9 +1,26 @@
-const x1 = document.querySelector('#x1_ip').value
-const y1 = document.querySelector('#y1_ip').value
-const xn = document.querySelector('#xn_ip').value
-const h = document.querySelector('#h_ip').value
-const eq = document.querySelector('#eq_ip').value
+var x1 = document.querySelector('#x1_ip')
+var y1 = document.querySelector('#y1_ip')
+var xn = document.querySelector('#xn_ip')
+var h = document.querySelector('#h_ip')
+var eq = document.querySelector('#eq_ip')
 const sub_but = document.querySelector('#sub_but')
 sub_but.addEventListener('click', () => {
-    console.log(x1.value)
+    x1 = x1.value
+    y1 = y1.value
+    xn = xn.value
+    h = h.value
+    Euler(x1.value, y1.value)
 })
+
+function Euler(x1, y1, xn, h) {
+    while (x1 < xn) {
+        y1 = y1 + h * Equation(x1, y1)
+        x1 = x1 + h
+        console.log(x1, y1)
+    }
+}
+
+function Equation(x, y) {
+    var equation = eval(eq.value)
+    return equation
+}
